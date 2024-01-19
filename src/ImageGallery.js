c5fa5032083ccdff1ba1ae7fb3677b8f4611f0f6
rawanAlzubaidi <101676826+rawanAlzubaidi@users.noreply.github.com>
2024-01-18 21:06:18 -0600

    fixed digital gallery phone view

--- src/ImageGallery.js
+++ src/ImageGallery.js
@@ -58,7 +58,8 @@ const ImageGallery = () => {
             <div className="container mt-4">
                 <div className="row">
                     {imageUrls.map((url, index) => (
-                        <div key={index} className="col-sm-2 col-md-4 col-lg-2 mb-4">
+                        // Update the class here to use col-4 for mobile and keep col-lg-2 for large screens
+                        <div key={index} className="col-4 col-sm-4 col-md-4 col-lg-2 mb-4">
                             <div className="polaroid" onClick={() => handleImageClick(index)}>
                                 <img src={url} alt={`Image ${index}`} className="img-fluid" />
                                 {/* <div className="caption">Image {index + 1}</div> */}
@@ -67,7 +68,6 @@ const ImageGallery = () => {
                     ))}
                 </div>
             </div>
-
             {/* Bootstrap Modal for enlarged image */}
             <Modal show={showModal} onHide={handleCloseModal} centered {...handlers}>
                 <Modal.Header closeButton>
